707a0899ea6f739ea41e23c0805d8f6b9295994d
syrlybajramazan <syrlybai056@gmail.com>
2023-12-11 16:03:38 +0600

    added - just create gpa fiunction

--- src/Task/Transcript.java
+++ src/Task/Transcript.java
@@ -16,7 +16,7 @@ import com.sun.source.tree.Tree;
  * @generated
  */
 public class Transcript {
-
+    TreeMap<String, Integer> grades = new TreeMap<>();
 
     /**
      * @generated
@@ -34,6 +34,13 @@ public class Transcript {
     /**
      * @generated
      */
+    public Transcript(){
+        grades.put("A", 4);
+        grades.put("B", 3);
+        grades.put("C", 2);
+        grades.put("D", 1);
+        grades.put("F", 0);
+    }
     public TreeMap<Course, Grade> getCourseGrades() {
         return this.courseGrades;
     }
@@ -47,7 +54,13 @@ public class Transcript {
      * @generated
      */
     public double getGpa() {
-        // TODO
+        double gpa = 0.000;
+        int cntOfCredits = 0;
+        for(Course c  : this.courseGrades.keySet()){
+            gpa += c.getCredit() * grades.get(courseGrades.get(c).getMark());
+            cntOfCredits += c.getCredit();
+        }
+        return gpa / cntOfCredits;
     }
 
     /**
