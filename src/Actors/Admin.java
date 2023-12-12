5960e1473aaf37d3d5507651effc2e92621e5b54
shynottea <kulekesh.m1705@mail.ru>
2023-12-12 22:35:51 +0600

    added documentation to Admin

--- src/Actors/Admin.java
+++ src/Actors/Admin.java
@@ -9,34 +9,41 @@ import java.util.*;
 import Interfaces.*;
 import Date.*;
 import Task.*;
+
 /**
- * @generated
+ * The Admin class extends the Employee class
  */
 public class Admin extends Employee {
 
     /**
-     * This field is used to describe the User is a researcher?
+     * The isResearcher field is used to determine whether the user is a researcher or not
      */
     public boolean isResearcher;
 
+    /**
+     * Default constructor for Admin
+     */
     public Admin(){
 
     }
 
+    /**
+     * Parameterized constructor for Admin
+     */
     public Admin(String name, String surname, String password, LANGUAGES lang, String department, double salary){
         super(name, surname, password, lang, department, salary);
         isResearcher = false;
     }
 
     /**
-     * @generated
+     * The seeLogAboutUser method allows viewing the user's log
      */
     public Vector<String> seeLogAboutUser(User user) throws NotAutorizedException {
         return user.getLogFiles();
     }
 
     /**
-     * @generated
+     * The removeUser method removes a user
      */
     public void removeUser(User user){
         DataBase dataBase = DataBase.getInstance();
@@ -44,30 +51,39 @@ public class Admin extends Employee {
     }
 
     /**
-     * @generated
+     * The UpdateUser method updates a user's data
      */
     public void UpdateUser(User oldUser, User newUser) throws NotAutorizedException {
         oldUser.changeToNewDataOfUser(newUser);
     }
 
     /**
-     * @generated
+     * The addUser method adds a user
      */
     public void addUser(User user){
         DataBase dataBase = DataBase.getInstance();
         dataBase.addUser(user);
     }
 
+    /**
+     * The isResearcher method checks if the user is a researcher
+     */
     @Override
     public boolean isResearcher() {
         return false;
     }
 
+    /**
+     * The setResearcher method sets the isResearcher field
+     */
     @Override
     public void setResearcher(boolean isResearcher) {
         this.isResearcher = isResearcher;
     }
 
+    /**
+     * The equals method checks if two Admin objects are equal
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -77,11 +93,17 @@ public class Admin extends Employee {
         return isResearcher == admin.isResearcher;
     }
 
+    /**
+     * The hashCode method returns the hash code of the Admin object
+     */
     @Override
     public int hashCode() {
         return Objects.hash(super.hashCode(), isResearcher);
     }
 
+    /**
+     * The toString method returns the string representation of the Admin object
+     */
     public String toString(){
         return super.toString();
     }
